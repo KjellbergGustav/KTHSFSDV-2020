df50178fdfcbd8ad19746adf15ba7df801f10929
Gustav Kjellberg <gustavkjellberg@Gustavs-MacBook-Pro.local>
2020-09-29 16:57:16 +0200

    added data and folder for exc3

---
+++ exc3/src/readData.py
@@ -0,0 +1,13 @@
+#import rosbag_pandas as rbpd
+import pandas as pd
+import pyrosbag
+import rosbag
+
+class ReadData():
+    def __init__(self):
+        self._data_path = 'data/data.bag'
+
+
+    def read_data(self):
+        df = rbpd.bag_to_dataframe(self._data_path)
+        df.head()
